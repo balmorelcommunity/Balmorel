41d26fa52a554fcdee90ba54278a25ada0b8a22d
Mathias157 <mathiasros@gmail.com>
2024-10-06 21:31:01 +0200

    Changed help message

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -121,7 +121,7 @@ def plot_costs(path_to_Balmorel: str, overwrite: bool, **kwargs):
 @click.option('--overwrite', is_flag=True, required=False, help='Overwrite previous collected results?')
 @click.option('--dark-style', is_flag=True, required=False, help='Dark style of the plot')
 @click.option('--plot-ext', type=str, required=False, default='.pdf', help='The extension of the plots, defaults to ".pdf"')
-@click.option('--path', type=str, required=False, default='.', help='Path to top level of Balmorel folders')
+@click.option('--path', type=str, required=False, default='.', help='Path to top level of Balmorel folders, defaults to "."')
 def main(gencap: bool, stocap: bool, obj: bool, all: bool, 
          overwrite: bool, dark_style: bool, plot_ext: str, path: str):
     
