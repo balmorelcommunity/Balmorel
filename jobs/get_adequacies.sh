4e02614c1da6a262a0349c741056e3e2e5c71a86
Mathias157 <mathiasros@proton.me>
2026-03-06 09:56:55 +0100

    Adjusted RAM needs for getting adequacy indicators

--- jobs/get_adequacies.sh
+++ jobs/get_adequacies.sh
@@ -9,9 +9,9 @@
 ### -- specify that the cores must be on the same host --
 #BSUB -R "span[hosts=1]"
 ### -- specify that we need 4GB of memory per core/slot --
-#BSUB -R "rusage[mem=2GB]"
+#BSUB -R "rusage[mem=4.5GB]"
 ### -- specify that we want the job to get killed if it exceeds 5 GB per core/slot --
-#BSUB -M 2GB
+#BSUB -M 4.5GB
 ### -- set walltime limit: hh:mm --
 #BSUB -W 1:00
 ### -- set the email address --
